88df6def688c75fbc37e7ac66fa0cdadf5604e5c
Jy <jysemoe@gmail.com>
2026-02-10 10:49:53 -0600

    Update heeler.execute.js

--- heeler.execute.js
+++ heeler.execute.js
@@ -273,6 +273,7 @@ let Heeler = {
 					}
 					
 					let codeOnlyTokens = codeOnlySegment.replaceAll('\n','').split(' ');
+					console.log(codeOnlySegment);
 					
 					let typePresent = codeOnlySegment.includes(" "+typeName+" ");
 					let nextExpectedPresent = (codeOnlySegment.includes(" "+nextExpected) || (codeOnlySegment !== '' && codeOnlyTokens[codeOnlyTokens.indexOf(typeName)+2].startsWith(nextExpected)) );	
