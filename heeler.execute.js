0985b60b77f622f9f850adac4f130c0fea3fab19
Jy <jysemoe@gmail.com>
2026-02-14 05:09:58 -0600

    Update heeler.execute.js

--- heeler.execute.js
+++ heeler.execute.js
@@ -15,7 +15,7 @@ let controlState = {
 	loop:true
 };
 Heeler.getScene = () => {return Object.assign({},Scene);};
-Heeler.swapScene = (incomingScene) => {Scene = incomingScene;};
+Heeler.swapScene = (incomingScene) => {controlState.loop = false; if(heelerExists()){ heeler_output.remove(); heeler_dump.remove(); }; Scene = incomingScene; };
 let heelerExists = () => {return typeof heeler_output !== 'undefined' && typeof ctx !== 'undefined';};
 let canvasMargins = {x:0,y:0};
 let adjustMargin = () => {if(heelerExists())canvasMargins = heeler_output.getBoundingClientRect();};
